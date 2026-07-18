42b8d2d13633ca4c6816c78f4f5b044ced7303a0
Chevdar <chaudar@evenbetgaming.com>
2026-07-18 21:31:54 +0300

    feat: export Container and Grid components along with Grid types from respective modules

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,9 @@
+export { Container } from './container';
+export { Counter } from './counter';
+export type {
+    GridCellTemplateContext,
+    GridCellValue,
+    GridColumn,
+    GridProps
+} from './grid';
+export { Grid } from './grid';
