124266d9af40a248a6a26429d659d6fea55645de
Chevdar <chaudar@evenbetgaming.com>
2026-07-23 16:18:18 +0300

    fix(style): remove unnecessary useMaxSize prop from Container

--- src/views/categories/page.tsx
+++ src/views/categories/page.tsx
@@ -181,7 +181,7 @@ export function CategoriesPage() {
     return (
         <>
             <main class={css.root}>
-                <Container class={css.page} useMaxSize>
+                <Container class={css.page}>
                     <Title>Категории</Title>
                     <header class={css.header}>
                         <div class={css.headerContent}>
