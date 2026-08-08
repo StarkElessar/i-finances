734c0c534aad45670fb88e27aaa08c2e9cc4594a
Chevdar <chaudar@evenbetgaming.com>
2026-08-08 07:58:17 +0300

    chore(web): update application bootstrap

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,16 +7,19 @@ import { AuthClient } from './features/auth';
 import { CategoryClient } from './features/categories';
 import { App } from './app';
 
-const root = document.getElementById('root');
+const root = document.querySelector('#root');
 
 if (root === null) {
 	throw new Error('Web application root element was not found.');
 }
 
-render(() => (
-	<App
-		accountClient={new AccountClient()}
-		authClient={new AuthClient()}
-		categoryClient={new CategoryClient()}
-	/>
-), root);
+render(
+	() => (
+		<App
+			accountClient={new AccountClient()}
+			authClient={new AuthClient()}
+			categoryClient={new CategoryClient()}
+		/>
+	),
+	root
+);
